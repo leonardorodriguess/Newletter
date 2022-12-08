9157d60f652256a974198818acad682eda924540
Leonardo <leo-feitosa@live.com>
2022-12-08 08:31:28 -0300

    Dark mode de acordo com o sistema operacional

--- src/components/ToggleTheme/ToggleTheme.jsx
+++ src/components/ToggleTheme/ToggleTheme.jsx
@@ -1,9 +1,19 @@
 import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
+import { useEffect } from "react";
 
 export function ToggleTheme(){
+  const systemPreference = window.matchMedia('(prefers-color-scheme: dark)').matches
+  const pageClasses = document.documentElement.classList
+  
   const toggle = () => {
-    document.documentElement.classList.toggle('dark')
+    pageClasses.toggle('dark');
   }
+
+  useEffect (() => {
+    systemPreference && (
+      pageClasses.add('dark')
+    );
+  });
   
   return (
     <div className="hidden sm:block">
