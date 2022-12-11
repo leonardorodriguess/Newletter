37fe8fc4a4b99736214cae260c58ed8eda701dbe
Leonardo Rodrigues <leo-feitosa@live.com>
2022-12-11 15:18:55 -0300

    Listando tags

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,7 +1,20 @@
 export function Article ({ title, text, tags, image, alt}) {
   return (
-    <div className="p-5 bg-gray-200 dark:bg-dark-200 rounded-xl sm:shadow-lg">
+    <div className="p-5 bg-gray-200 dark:bg-dark-200 rounded-xl sm:shadow-lg gap-2">
+      
       <h3 className="text-xl text-custom-200 dark:text-gray-200 font-bold flex flex-col items-center">{title}</h3>
+
+      <div className="w-full justify-end gap-2 pr-5 hidden sm:flex">
+        {tags.map((tag, index) => 
+          <span 
+            key={index}
+            className="bg-custom-100 dark:bg-dark-100 px-4 py-1 rounded-full text-gray-200 font-bold text-xs uppercase"
+          >
+            {tag}
+          </span>
+        )}
+      </div>
+
       <div className="grid gap-1">
         {
           text.map((context, index) => 
@@ -14,6 +27,7 @@ export function Article ({ title, text, tags, image, alt}) {
           )
         }
       </div>
+
       { image && <img className="sm:p-4" src={image} />}
       { image && alt && <span className="sr-only">{alt}</span>}
     </div>
