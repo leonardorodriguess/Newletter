05f876e09ffe487fd0145fce4ac7d05ab0c95a95
Leonardo Rodrigues <leo-feitosa@live.com>
2022-12-11 14:44:25 -0300

    Inserido imagem

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,6 +1,6 @@
 export function Article ({ title, text, tags, image, alt}) {
   return (
-    <div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg">
+    <div className="p-5 bg-gray-200 dark:bg-dark-200 rounded-xl sm:shadow-lg">
       <h3 className="text-xl text-custom-200 dark:text-gray-200 font-bold flex flex-col items-center">{title}</h3>
       <div className="grid gap-1">
         {
@@ -14,6 +14,8 @@ export function Article ({ title, text, tags, image, alt}) {
           )
         }
       </div>
+      { image && <img className="sm:p-4" src={image} />}
+      { image && alt && <span className="sr-only">{alt}</span>}
     </div>
   );
 }
--- src/components/ArticleList/ArticleList.jsx
+++ src/components/ArticleList/ArticleList.jsx
@@ -3,7 +3,7 @@ import { Article } from "../Article/Article";
 
 export function ArticleList () {
   return (
-    <div className="mt-5 sm:mt-0 grid gap-5 m-auto max-w-2xl sm:grid-cols-2 lg:grid-cols-3 lg:max-w-[1000px]">
+    <div className="mt-5 sm:mt-0 grid gap-5 mx-auto justify-center px-5 max-w-2xl sm:grid-cols-2 lg:grid-cols-3 lg:max-w-[1000px]">
       {
         data.map((props, index) => 
           <p key={index}><Article key={index} {...props} /></p>
