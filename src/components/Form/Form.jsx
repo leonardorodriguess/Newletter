3b2c65cf6be16da31d09d5ce8e78faec95a640d4
Leonardo <leo-feitosa@live.com>
2022-12-12 07:50:42 -0300

    Estilização Botao

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -15,9 +15,10 @@ export function Form ({ onSubmit }) {
       <form onSubmit={safeSubmit} className="bg-gray-300 dark:bg-gray-500 flex flex-col gap-10 px-5 py-8 rounded-md w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl">
 
         <Input type="text" required placeholder="Insira seu nome..." />
-        <Input type="text" required placeholder="Insira seu e-mail..." />
+        <Input type="email" required placeholder="Insira seu e-mail..." />
 
-        <button type="submit" className="bg-custom-100 dark:bg-dark-100  text-gray-200 rounded-full px-5 py-1 uppercase">Seguir</button>
+        <button type="submit" className="bg-custom-100 dark:bg-dark-100  text-gray-200 rounded-full px-5 py-1 uppercase 
+        hover:animate-pulse hover:shadow-md hover:shadow-gray-500 hover:dark:shadow-black">Seguir</button>
 
       </form>
 
